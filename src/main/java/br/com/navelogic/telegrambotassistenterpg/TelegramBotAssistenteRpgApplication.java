236d511afdcbfa7db78a7e067138561fef3cb50b
Arian Weslley <weslleysantoszbd@gmail.com>
2024-11-09 11:04:24 -0300

    Modelo TelegramBuilder

--- src/main/java/br/com/navelogic/telegrambotassistenterpg/TelegramBotAssistenteRpgApplication.java
+++ src/main/java/br/com/navelogic/telegrambotassistenterpg/TelegramBotAssistenteRpgApplication.java
@@ -1,7 +1,11 @@
 package br.com.navelogic.telegrambotassistenterpg;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 @SpringBootApplication
 public class TelegramBotAssistenteRpgApplication {
@@ -9,5 +13,19 @@ public class TelegramBotAssistenteRpgApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotAssistenteRpgApplication.class, args);
 	}
-
+}
+@Component
+class TelegramBotAssistenteRpgInitializer implements CommandLineRunner {
+	@Value("${telegram.bot.token}")
+	private String botToken;
+	@Override
+	public void run(String... args) throws Exception {
+		try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+			botsApplication.registerBot(botToken, new RPGBot(botToken));
+			System.out.println("RPGBot está rodando...");
+			Thread.currentThread().join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
