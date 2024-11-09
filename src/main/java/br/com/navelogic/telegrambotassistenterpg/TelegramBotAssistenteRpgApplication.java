40f0caeea3fd2afe9fc85c05c630df646150fed0
Arian Weslley <weslleysantoszbd@gmail.com>
2024-11-09 10:53:00 -0300

    Iniciando part2

---
+++ src/main/java/br/com/navelogic/telegrambotassistenterpg/TelegramBotAssistenteRpgApplication.java
@@ -0,0 +1,13 @@
+package br.com.navelogic.telegrambotassistenterpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAssistenteRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotAssistenteRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/navelogic/telegrambotassistenterpg/TelegramBotAssistenteRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.navelogic.telegrambotassistenterpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAssistenteRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
